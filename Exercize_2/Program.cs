623d263c1aa7cc6336cd79468f1c5f24aa05694b
Kristina <abrkriss@gmail.com>
2022-11-19 14:02:22 +0300

    Exercize_2

---
+++ Exercize_2/Program.cs
@@ -0,0 +1,17 @@
+﻿void Exercize2()
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+     if ((99 < n) &  (n < 1000))
+    {
+    int ones = n % 10;
+    Console.WriteLine("Третья цифра заданного числа " + ones);
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное число");
+    }
+}
+Exercize2();
+
